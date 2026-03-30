1d19f37c15607eb60d539e1b9242166adca59dcb
Dhruv Kuchhal <133423867+FighterDhruv8@users.noreply.github.com>
2026-03-30 21:02:06 +0530

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.java
@@ -0,0 +1,35 @@
+class MinStack
+{
+
+    Stack<Integer> st, minst;
+
+    public MinStack()
+    {
+        this.st = new Stack<Integer>();
+        this.minst = new Stack<Integer>();
+    }
+    
+    public void push(int val)
+    {
+        if (this.st.isEmpty() || this.minst.peek() >= val)
+            this.minst.push(val);
+        this.st.push(val);
+    }
+    
+    public void pop()
+    {
+        int a = this.st.pop();
+        if (this.minst.peek() == a)
+            this.minst.pop();
+    }
+    
+    public int top()
+    {
+        return this.st.peek();
+    }
+    
+    public int getMin()
+    {
+        return this.minst.peek();
+    }
+}
